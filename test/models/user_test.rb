dd22edbb55c23f9c99d42f11498c0673e6b1c0a6
Hikari Yanagihara <Hikari@Hikari-no-MacBook-Pro.local>
2018-06-13 01:34:27 +0900

    initial commit

---
+++ test/models/user_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "hikari", email: "ave.nxson@gmail.com",
+      password: "hogehoge", password_confirmation: "hogehoge")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "email addresses should be unique" do
+    # dup:同じ属性のユーザを複製
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+end
