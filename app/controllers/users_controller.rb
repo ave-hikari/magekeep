2f8a0dc2a4581442c8975d5e6ddcae105f9b0641
ave-hikari <ave.nxson@gmail.com>
2018-06-16 15:47:29 +0900

    test書いたりした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "ユーザ登録に成功しました！"
       redirect_to root_url
     else
       render 'new'
