95f1d3de960bdd680f99a3ab21d5c36b6e48ddb5
ave-hikari <ave.nxson@gmail.com>
2018-07-08 02:10:30 +0900

    写真をフォームからuploadできるようにした

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,13 @@
 class Picture < ApplicationRecord
   # photoをattachファイルとする。stylesで画像サイズを定義できる
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :photo,
+    styles: { medium: "300x300>", thumb: "100x100>" },
+    url: "/assets/arts/:id/:style/:basename.:extension",
+    path: "#{Rails.root}/public/assets/arts/:id/:style/:basename.:extension" # サーバ上の画像保存先パス
 
   # ファイルの拡張子を指定
-  validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  validates_attachment :photo,
+    presence: true,
+    less_than: 5.megabytes, # ファイルサイズのチェック
+    content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
